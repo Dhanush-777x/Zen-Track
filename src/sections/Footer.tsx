9752ccd1dbd7625b37066922d2f1468a550174b7
dhanushsm <dhanushmani1504@gmail.com>
2024-11-30 10:49:35 +0530

    Updated copyright

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -29,7 +29,7 @@ export const Footer = () => {
           <SocialYoutube />
         </div>
         <p className="mt-6">
-          &copy; 2024 Dhanush S M, Inc All rights reserved.
+          &copy; 2024 Zen Trck, Inc All rights reserved.
         </p>
       </div>
     </footer>
